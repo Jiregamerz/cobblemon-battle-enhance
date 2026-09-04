cd68510c1f12a0320f522131ef2e9531feaba7d3
ArkoPratimDas <arkopratimdas@gmail.com>
2026-09-04 11:45:35 +0530

    Simplify ClientPlayerEntityMixin (empty, removed from config)

--- src/main/java/com/battleenhance/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/com/battleenhance/mixin/ClientPlayerEntityMixin.java
@@ -9,21 +9,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LocalPlayer.class)
 public abstract class ClientPlayerEntityMixin {
-
-    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
-    private void onSendChatMessage(String message, CallbackInfo ci) {
-        if (message.startsWith("/battleenhance ")) {
-            String[] parts = message.split(" ");
-            if (parts.length >= 2) {
-                String command = parts[1];
-                if (command.equals("start")) {
-                    BattleEnhanceMod.getLOGGER().info("Manual battle start requested");
-                    ci.cancel();
-                } else if (command.equals("stop")) {
-                    BattleEnhanceMod.INSTANCE.endBattle();
-                    ci.cancel();
-                }
-            }
-        }
-    }
+    // Player input is handled by PlayerEntityMixin and PokemonController.kt
 }
